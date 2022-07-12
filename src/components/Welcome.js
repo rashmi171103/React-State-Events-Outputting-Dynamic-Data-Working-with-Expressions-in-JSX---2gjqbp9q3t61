57a0e3187736dea3e13b2aaed3e785d043ddbda5
rashmi171103 <rashmi171103@gmail.com>
2022-07-12 17:00:01 +0530

    ab toh ho ja

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,8 +6,8 @@ const  Welcome = (props) => {
 
     return (
         <div>
-<h1>Hey!{props.name} </h1>
-<h2>Welcome to Newton School</h2>
+<h1>Hey !{props.name} </h1>
+<h2>Welcome to Newton School.</h2>
         </div>
 
     )
