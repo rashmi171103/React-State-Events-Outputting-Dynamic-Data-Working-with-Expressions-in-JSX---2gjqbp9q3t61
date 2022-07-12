3cfcb45c24c81def8dcd51bb3d06d592f0d2f436
rashmi171103 <rashmi171103@gmail.com>
2022-07-12 16:49:01 +0530

    Done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,16 @@
 // write code for Welcome component here
+import React from "react";
+
+
+const  Welcome = (props) => {
+
+    return (
+<h1>
+    Hey!{props.name} 
+<h2>Welcome to Newton School</h2>
+</h1>
+
+    )
+}
+
+export default Welcome;
