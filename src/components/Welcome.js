c6085c53225060f47406c3e198b1191be382a7b3
rashmi171103 <rashmi171103@gmail.com>
2022-07-12 16:56:10 +0530

    N done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,10 +5,10 @@ import React from "react";
 const  Welcome = (props) => {
 
     return (
-<h1>
-    Hey!{props.name} 
+        <div>
+<h1>Hey!{props.name} </h1>
 <h2>Welcome to Newton School</h2>
-</h1>
+        </div>
 
     )
 }
